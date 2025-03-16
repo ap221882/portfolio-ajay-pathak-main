8cc002da02b0e035ff6093cb1ecdb57a4a68238d
ajay <ap221882@gmail.com>
2025-03-16 14:10:02 +0530

    feat: added new blog

--- app/data.ts
+++ app/data.ts
@@ -76,25 +76,25 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
 
 export const BLOG_POSTS: BlogPost[] = [
   {
-    title: "Exploring the Intersection of Design, AI, and Design Engineering",
-    description: "How AI is changing the way we design",
+    title: "WeakMap vs Map",
+    description: "Utilise garbage collection in JavaScript",
     link: "/blog/exploring-the-intersection-of-design-ai-and-design-engineering",
     uid: "blog-1",
   },
-  {
-    title: "Why I left my job to start my own company",
-    description:
-      "A deep dive into my decision to leave my job and start my own company",
-    link: "/blog/exploring-the-intersection-of-design-ai-and-design-engineering",
-    uid: "blog-2",
-  },
-  {
-    title: "What I learned from my first year of freelancing",
-    description:
-      "A look back at my first year of freelancing and what I learned",
-    link: "/blog/exploring-the-intersection-of-design-ai-and-design-engineering",
-    uid: "blog-3",
-  },
+  // {
+  //   title: "Why I left my job to start my own company",
+  //   description:
+  //     "A deep dive into my decision to leave my job and start my own company",
+  //   link: "/blog/exploring-the-intersection-of-design-ai-and-design-engineering",
+  //   uid: "blog-2",
+  // },
+  // {
+  //   title: "What I learned from my first year of freelancing",
+  //   description:
+  //     "A look back at my first year of freelancing and what I learned",
+  //   link: "/blog/exploring-the-intersection-of-design-ai-and-design-engineering",
+  //   uid: "blog-3",
+  // },
 ];
 
 export const SOCIAL_LINKS: SocialLink[] = [
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { XIcon } from "lucide-react";
 import { motion } from "motion/react";
+import Link from "next/link";
 
+import { AnimatedBackground } from "@/components/ui/animated-background";
 import { Magnetic } from "@/components/ui/magnetic";
 import {
   MorphingDialog,
@@ -13,7 +15,7 @@ import {
 import FullPageScroll from "@/components/ui/scroll";
 import { Spotlight } from "@/components/ui/spotlight";
 
-import { EMAIL, SOCIAL_LINKS, WORK_EXPERIENCE } from "./data";
+import { BLOG_POSTS, EMAIL, SOCIAL_LINKS, WORK_EXPERIENCE } from "./data";
 
 const VARIANTS_CONTAINER = {
   hidden: { opacity: 0 },
@@ -213,41 +215,41 @@ export default function Personal() {
           </div>
         </motion.section>
 
-        {/* <motion.section
-        variants={VARIANTS_SECTION}
-        transition={TRANSITION_SECTION}
-      >
-        <h3 className="mb-3 text-lg font-medium">Blog</h3>
-        <div className="flex flex-col space-y-0">
-          <AnimatedBackground
-            enableHover
-            className="h-full w-full rounded-lg bg-zinc-100 dark:bg-zinc-900/80"
-            transition={{
-              type: 'spring',
-              bounce: 0,
-              duration: 0.2,
-            }}
-          >
-            {BLOG_POSTS.map((post) => (
-              <Link
-                key={post.uid}
-                className="-mx-3 rounded-xl px-3 py-3"
-                href={post.link}
-                data-id={post.uid}
-              >
-                <div className="flex flex-col space-y-1">
-                  <h4 className="font-normal dark:text-zinc-100">
-                    {post.title}
-                  </h4>
-                  <p className="text-zinc-500 dark:text-zinc-400">
-                    {post.description}
-                  </p>
-                </div>
-              </Link>
-            ))}
-          </AnimatedBackground>
-        </div>
-      </motion.section> */}
+        <motion.section
+          variants={VARIANTS_SECTION}
+          transition={TRANSITION_SECTION}
+        >
+          <h3 className="mb-3 text-lg font-medium">Blog</h3>
+          <div className="flex flex-col space-y-0">
+            <AnimatedBackground
+              enableHover
+              className="h-full w-full rounded-lg bg-zinc-100 dark:bg-zinc-900/80"
+              transition={{
+                type: "spring",
+                bounce: 0,
+                duration: 0.2,
+              }}
+            >
+              {BLOG_POSTS.map((post) => (
+                <Link
+                  key={post.uid}
+                  className="-mx-3 rounded-xl px-3 py-3"
+                  href={post.link}
+                  data-id={post.uid}
+                >
+                  <div className="flex flex-col space-y-1">
+                    <h4 className="font-normal dark:text-zinc-100">
+                      {post.title}
+                    </h4>
+                    <p className="text-zinc-500 dark:text-zinc-400">
+                      {post.description}
+                    </p>
+                  </div>
+                </Link>
+              ))}
+            </AnimatedBackground>
+          </div>
+        </motion.section>
 
         <motion.section
           variants={VARIANTS_SECTION}
